86ee080343a41bd4297e8bc11831974036f99163
Ana Villas <anacarolina.vbp@gmail.com>
2026-03-04 00:03:49 -0300

    🔐 feat: implement stateless session policy

--- pub/src/main/java/com/grupo_5/pub/Infra/Security/SecurityConfig.java
+++ pub/src/main/java/com/grupo_5/pub/Infra/Security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
 
                 .anyRequest().authenticated();
             )
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
